c75e7f3c31b732339cd8c0e112c673f2cbf6d782
Jillian Fusco <fuscojillian@gmail.com>
2024-10-19 18:14:55 -0400

    Task 4 complete, passing JobTest and task 4 autograder

--- src/test/java/org/launchcode/techjobs/oo/JobTest.java
+++ src/test/java/org/launchcode/techjobs/oo/JobTest.java
@@ -1,5 +1,50 @@
 package org.launchcode.techjobs.oo;
 
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
 public class JobTest {
     //TODO: Create your unit tests here
+
+    @Test
+    public void testSettingJobId() {
+        Job job1 = new Job();
+        Job job2 = new Job();
+
+        assertNotEquals(job1.getId(), job2.getId());
+    }
+
+    @Test
+    public void testJobConstructorSetsAllFields() {
+        Job job = new Job(
+                "Product tester",
+                new Employer("ACME"),
+                new Location("Desert"),
+                new PositionType("Quality control"),
+                new CoreCompetency("Persistence"));
+
+        assertTrue(job.getEmployer() instanceof Employer);
+        assertTrue(job.getLocation() instanceof Location);
+        assertTrue(job.getPositionType() instanceof PositionType);
+        assertTrue(job.getCoreCompetency() instanceof CoreCompetency);
+        assertTrue(job.getName() instanceof String);
+
+        assertEquals("Product tester", job.getName());
+        assertEquals("ACME", job.getEmployer().getValue());
+        assertEquals("Desert", job.getLocation().getValue());
+        assertEquals("Quality control", job.getPositionType().getValue());
+        assertEquals("Persistence", job.getCoreCompetency().getValue());
+    }
+
+    @Test
+    public void testJobsForEquality() {
+        Job job1 = new Job("Junior Web Dev", new Employer ("DevCorp"), new Location("Philadelphia"), new PositionType("Junior Web Dev"), new CoreCompetency("JavaScript"));
+
+        Job job2 = new Job("Junior Web Dev", new Employer ("DevCorp"), new Location("Philadelphia"), new PositionType("Junior Web Dev"), new CoreCompetency("JavaScript"));
+
+        assertFalse(job1.equals(job2));
+    }
+
 }
